056d477420ed0953f0839ddb712d14466ae64c84
Matheus Pereira <matheus.tecinfor98@gmail.com>
2021-09-14 23:15:17 -0300

    test: crud reports testes

--- tests/Feature/ReportTest.php
+++ tests/Feature/ReportTest.php
@@ -18,13 +18,44 @@ class ReportTest extends TestCase
      */
     public function testBasicTest()
     {
-        $response = $this->post('/api/v1/reports', [
+        $data = [
             'external_id' => 'ASV3455645FSAS',
             'title' => 'NEWS TITLE',
             'url' => 'http://www.test.com',
             'summary' => 'test test test test test test test test test test test test test test test'
-        ]);
+        ];
+
+        $response = $this->post('/api/v1/reports', $data);
 
         $response->assertStatus(201);
+    }
+
+    /**
+     * A basic update report test example.
+     *
+     * @return void
+     */
+    public function testUpdateReport()
+    {
+        $response = $this->put('/api/v1/reports/1', [
+            'external_id' => 'ASV3455645FSAS',
+            'title' => 'NEWS TITLE UPDATED',
+            'url' => 'http://www.test.com/updated',
+            'summary' => 'Summary updated'
+        ]);
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * A basic not found id report test example.
+     *
+     * @return void
+     */
+    public function testNotFoundReport()
+    {
+        $response = $this->get('/api/v1/reports/99');
+
+        $response->assertStatus(400);
     }
 }
